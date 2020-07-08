1eac6761dcda5c78aa238da24b3c991f35a34c91
ZigiJesse-Jackson <jzjackson995@gmail.com>
2020-07-08 18:50:38 +0000

    Lab00 update

--- Lab00.py
+++ Lab00.py
@@ -1,5 +1,13 @@
 '''Authors: Jesse-Jackson Zigi and Bright Okrah'''
 
+
+'''
+    This is just a brief inheritance overview with Python
+    To initialize any of the classes, just pass a string 
+    as an argument. Each class has an overridden get_specs()
+    and display_specs() method. The get_specs() method returns
+    a string while the display_specs() method prints out a string.
+'''
 #Inheritance practice
 class Computer:
     _specs = ""
@@ -8,7 +16,7 @@ class Computer:
         self._specs = specs
 
     def get_specs(self):
-        return _specs
+        return self._specs
 
     def display_specs(self):
         print("Specs:{0}".format(self._specs))
@@ -31,6 +39,9 @@ class Desktop(Computer):
         print("Desktop specs: {0}".format(self._specs))
 
 
+'''
+You can test any of the classes this way.
+'''
 laptop = Laptop("3GB Ram")
 laptop.display_specs()
 
