4e73deac1eedfb20edead378e2b68967dfa34ff7
ZigiJesse-Jackson <jzjackson995@gmail.com>
2020-08-02 20:35:43 +0000

    Project assignment phase 1 Hashtable with direct chaining and linked-list

---
+++ DirectChainingHashTable.py
@@ -0,0 +1,64 @@
+'''
+    Authors: Jesse-Jackson Zigi
+             Bright Okrah
+'''
+import random
+from LinkedList import LinkList
+
+
+
+class HashTable():
+    _capacity = 509
+    _table = _capacity * [LinkList(None)]
+    _num = 0
+
+    def __init__(self):
+        self._capacity = 509
+        self._table = self._capacity * [LinkList(None)]
+        self.num = 0
+
+    def __hash(self, key):
+        key *= key # square key
+        skey = str(key) # turn key to string to slice
+        if len(skey) >= 3:
+            hashk = int(skey[(len(skey)//2)-1:(len(skey)//2)+1]) # retrieve middle values
+            return hashk % self._capacity # hash key and return slot position for key
+        # If len of key is less than 3, just take numbers and hash
+        hashk = int(skey)  # retrieve middle values
+        return hashk % self._capacity
+
+
+
+    def add(self, key, val):
+        index = self.__hash(key)
+        self._table[index].push(val)
+
+
+    def __len__(self):
+        return self.num
+
+    def printIndex(self, key):
+        index = self.__hash(key)
+        if self._table[index].head.data != None:
+            print(self._table[index].print())
+            return
+        return
+
+    def printHash(self, key):
+        print(self.__hash(key))
+
+
+table = HashTable()
+
+table.add(65, 34)
+table.add(35, 34)
+table.add(5, 36)
+table.add(4, 38)
+table.add(36, 34)
+table.add(5, 34)
+
+table.printIndex(5)
+
+
+
+
---
+++ LinkedList.py
@@ -0,0 +1,48 @@
+
+class LinkNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkList:
+    head = None
+
+    def __init__(self, data):
+        self.head = LinkNode(data)
+
+    def push(self, data):
+        if self.head.data == None:
+            self.head.data = data
+            return
+
+        node = self.head
+        while node.next != None:
+            node = node.next
+        node.next = LinkNode(data)
+        return
+
+    # Removes first occurence of data item
+    def remove(self, data):
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        node = self.head
+        while node.next.data != data:
+            node = node.next
+        node.next = node.next.next
+        return
+
+    def print(self):
+        node = self.head
+        while node.next != None:
+            print(node.data, end=" ")
+            node = node.next
+        print(node.data)
+        return
+
+
+
+
+
+
